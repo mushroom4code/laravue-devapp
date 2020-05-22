cc5b034ebae48ed14b4feea028d2a47a8633b48e
mushroom4code <opaaawhoizdiz@gmail.com>
2020-05-22 17:30:09 +0300

    secure asset2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Laravue</title>
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
-  <link rel="stylesheet" href="{{ asset('css/vue-style.css') }}" type="text/css">
+  <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}" type="text/css">
+  <link rel="stylesheet" href="{{ secure_asset('css/vue-style.css') }}" type="text/css">
   <script type="text/javascript">
     window.vuebnb_server_data = "{!! addslashes(json_encode($data)) !!}"
     window.csrf_token = "{{ csrf_token() }}"
