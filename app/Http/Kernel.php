bf3b61c89506c49514f09b9b12288e19b580a758
mushroom4code <opaaawhoizdiz@gmail.com>
2020-05-24 17:07:07 +0300

    mda2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\SecureRedirections::class,
+        
     ];
 
     /**
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
 
-            \App\Http\Middleware\SecureRedirections::class,
+
         ],
 
         'api' => [
