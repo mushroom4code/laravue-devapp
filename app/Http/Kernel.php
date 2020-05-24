49c62b31b7ba242867951ba2bb4621cb2d79eb0f
mushroom4code <opaaawhoizdiz@gmail.com>
2020-05-24 12:25:56 +0300

    https redirects

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\SecureRedirections::class,
     ];
 
     /**
@@ -36,6 +37,8 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+
+            \App\Http\Middleware\SecureRedirections::class,
         ],
 
         'api' => [
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Laravue</title>
-  <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}" type="text/css">
-  <link rel="stylesheet" href="{{ secure_asset('css/vue-style.css') }}" type="text/css">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+  <link rel="stylesheet" href="{{ asset('css/vue-style.css') }}" type="text/css">
   <script type="text/javascript">
     window.vuebnb_server_data = "{!! addslashes(json_encode($data)) !!}"
     window.csrf_token = "{{ csrf_token() }}"
@@ -14,6 +14,6 @@
 </head>
 <body>
 <div id="app"></div>
-<script src="{{ secure_asset('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
