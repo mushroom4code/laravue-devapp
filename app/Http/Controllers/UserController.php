37b4265b93c22c68ccdd3200cc6e71ed1f15b44f
mushroom4code <opaaawhoizdiz@gmail.com>
2020-05-24 18:56:23 +0300

    ffafa

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
