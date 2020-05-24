85423db93ff96e8e088186f0961279a2f37e0398
mushroom4code <opaaawhoizdiz@gmail.com>
2020-05-24 18:23:12 +0300

    fas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
     {
         error_log('Some message here.');
         $id = Input::get('id');
-        $user = Auth::user();
+        
         $saved = $user->saved;
         $key = array_search($id, $saved);
         if ($key === FALSE) {
